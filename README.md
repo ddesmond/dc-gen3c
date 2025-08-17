aef903b904b562d768109e8187a72e37133989ae
Aleks <crni.aki@gmail.com>
2025-08-17 07:44:32 +0200

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+### Yum yum GEN3C
+
+### Description
+runs on 12.6
+### Usage
+```
+curl https://raw.githubusercontent.com/ddesmond/dc-gen3c/refs/heads/main/deploy.sh | bash 
+```
+
+or from DC dashboard as a script or clone / bash
--- run_gui.sh
+++ run_gui.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+cd /opt/gen3c
+
+conda activate cosmos-predict1
+cd gui
+pip install fastapi
+pip install -r ./requirements.txt
+
 cd /opt/gen3c/gui
 CUDA_HOME=$CONDA_PREFIX fastapi dev --no-reload ./api/server.py --host 0.0.0.0
